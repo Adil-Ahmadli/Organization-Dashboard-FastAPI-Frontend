6e29242c92abff81261ff21e07271e81c2f41a51
Adil-Ahmadli <adil.ahmadli@proton.me>
2024-02-27 22:25:35 +0300

    feat: basic frontend application

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import Items from "./pages/items/Items";
+import Users from "./pages/users/Users";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/items", element: <Items /> },
+        { path: "/users", element: <Users /> },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span></span>
+      <span>Dashboard 2024</span>
+    </div>
+  );
+};
+
+export default Footer;
