ba765c68f12e6ba409da3c52cae26c1c18721418
Adil-Ahmadli <adil.ahmadli@proton.me>
2024-03-02 19:40:09 +0300

    feat: basic user cannot see Logs

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -4,10 +4,18 @@ import { menu } from "../../data";
 import { UserContext } from "../../context/UserContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const Menu = () => {
   const navigate = useNavigate();
   const [token, setToken] = useContext(UserContext);
+  const decoded = jwtDecode(token);
+  
+  if (decoded.employee_role === "user") {
+    menu[1].listItems = menu[1].listItems.filter(
+      (listItem) => listItem.title !== "Logs"
+    );
+  }
 
   const handleLogout = () => {
     setToken(null);
@@ -31,10 +39,7 @@ const Menu = () => {
       <div className="item">
         <span className="title">Log Out</span>
         <div onClick={handleLogout} className="listItem">
-          <img
-            src="../../../public/logout.svg"
-            alt=""
-          />
+          <img src="logout.svg" alt="" />
           <span className="listItemTitle">Log Out</span>
         </div>
       </div>
