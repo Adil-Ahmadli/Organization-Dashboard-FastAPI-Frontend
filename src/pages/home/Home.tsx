bc983dcf6a3df02acd19c1f6a49ebe2c52b5b741
Adil-Ahmadli <adil.ahmadli@proton.me>
2024-03-03 18:07:42 +0300

    feat: Responsive design and various Charts in the Homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,15 @@
 import "./home.scss";
 import TopBox from "../../components/topBox/TopBox";
+import ChartBox from "../../components/chartBox/ChartBox";
+
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 
 const Home = () => {
   return (
@@ -7,11 +17,19 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2"></div>
-      <div className="box box3"></div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
       <div className="box box4"></div>
-      <div className="box box5"></div>
-      <div className="box box6"></div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
       <div className="box box7"></div>
       <div className="box box8"></div>
       <div className="box box9"></div>
