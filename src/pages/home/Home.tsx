feb8d6d6b2f5465b05fccdde971c08fdbca6ad0d
Adil-Ahmadli <adil.ahmadli@proton.me>
2024-03-03 17:50:14 +0300

    feat: Grid Cells in the Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,22 @@
-import "./home.scss"
- 
+import "./home.scss";
+import TopBox from "../../components/topBox/TopBox";
+
 const Home = () => {
   return (
-    <div className="home">Home</div>
-  )
-}
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2"></div>
+      <div className="box box3"></div>
+      <div className="box box4"></div>
+      <div className="box box5"></div>
+      <div className="box box6"></div>
+      <div className="box box7"></div>
+      <div className="box box8"></div>
+      <div className="box box9"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
